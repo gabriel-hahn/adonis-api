798d3abce2d131668e00fad8abb842af0c4a6660
Gabriel Hahn Schaeffer <gabriel_hahn@hotmail.com>
2019-07-15 21:51:24 -0300

    Initial route for user session

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -3,14 +3,13 @@
 const User = use('App/Models/User')
 
 class UserController {
-    async store({ request }) {
-        const data = request.only(['username', 'email', 'password'])
-        console.log(data)
+  async store ({ request }) {
+    const data = request.only(['username', 'email', 'password'])
 
-        const user = await User.create(data)
+    const user = await User.create(data)
 
-        return user
-    }
+    return user
+  }
 }
 
 module.exports = UserController
