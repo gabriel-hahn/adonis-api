daa3760f5b457954587df1815250200d14b74acb
Gabriel Hahn Schaeffer <gabriel_hahn@hotmail.com>
2019-07-15 22:37:10 -0300

    Forgot password controller

---
+++ app/Controllers/Http/ForgotPasswordController.js
@@ -0,0 +1,22 @@
+'use strict'
+
+const crypto = require('crypto')
+const User = use('App/Models/User')
+
+class ForgotPasswordController {
+  async store ({ request, response }) {
+    try {
+      const email = request.input('email')
+      const user = await User.findByOrFail('email', email)
+
+      user.token = crypto.randomBytes(10).toString('hex')
+      user.token_created_at = new Date()
+
+      await user.save()
+    } catch (err) {
+      return response.status(err.status).send({ error: { message: 'Something wrong happened! It this email exists?' } })
+    }
+  }
+}
+
+module.exports = ForgotPasswordController
