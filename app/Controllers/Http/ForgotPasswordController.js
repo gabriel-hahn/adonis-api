7a80af8edb4bf7e0d4fd5af9d6d02de9c79af5ff
Gabriel Hahn Schaeffer <gabriel_hahn@hotmail.com>
2019-07-15 23:56:49 -0300

    Email configuration and password recovery template

--- app/Controllers/Http/ForgotPasswordController.js
+++ app/Controllers/Http/ForgotPasswordController.js
@@ -2,6 +2,7 @@
 
 const crypto = require('crypto')
 const User = use('App/Models/User')
+const Mail = use('Mail')
 
 class ForgotPasswordController {
   async store ({ request, response }) {
@@ -13,6 +14,16 @@ class ForgotPasswordController {
       user.token_created_at = new Date()
 
       await user.save()
+
+      await Mail.send(
+        ['emails.forgot_password'],
+        { email, token: user.token, link: `${request.input('redirect_url')}?token=${user.token}` },
+        message => {
+          message.to(user.email)
+            .from('gabriel_hahn@hotmail.com', 'Gabriel Hahn Schaeffer')
+            .subject('Password recovery')
+        }
+      )
     } catch (err) {
       return response.status(err.status).send({ error: { message: 'Something wrong happened! It this email exists?' } })
     }
