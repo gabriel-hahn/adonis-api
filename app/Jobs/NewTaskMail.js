acc90704a1bf5db4cfca5539b5ac9f6db6fd4099
Gabriel Hahn Schaeffer <gabriel_hahn@hotmail.com>
2019-07-21 20:37:52 -0300

    Redis and Kue config

---
+++ app/Jobs/NewTaskMail.js
@@ -0,0 +1,35 @@
+'use strict'
+
+const Mail = use('Mail')
+const Helpers = use('Helpers')
+
+class NewTaskMail {
+  static get concurrency () {
+    return 1
+  }
+
+  static get key () {
+    return 'NewTaskMail-job'
+  }
+
+  async handle ({ email, username, title, file }) {
+    await Mail.send(
+      ['emails.new_task'],
+      { username, title, hasAttachment: !!file },
+      message => {
+        message
+          .to(email)
+          .from('gabriel_hahn@hotmail.com', 'Gabriel Hahn Schaeffer')
+          .subject('New task for you')
+
+        if (file) {
+          message.attach(Helpers.tmpPath(`uploads/${file.file}`), {
+            filename: file.name
+          })
+        }
+      }
+    )
+  }
+}
+
+module.exports = NewTaskMail
